9b1148476728b889b061c8a6cbad7c50986a6b4a
ericjhkim <erickim2010@hotmail.com>
2022-04-11 14:53:55 -0700

    V2
    Version for new Nano IOT

--- README.txt
+++ README.txt
@@ -10,3 +10,10 @@ External libraries (install as required):
 Motor controller: https://github.com/pololu/qik-arduino
 Averaging filter: https://github.com/kbowerma/arduino/blob/master/libraries/Average/Average.h
 6DOF IMU: https://github.com/arduino-libraries/Arduino_LSM6DS3
+
+Version 2 for new Nano boards:
+For the new Arduino Nano 33 IOT, SoftwareSerial.h is incompatible (and unnecessary). The board has a hardware serial on pins 0 and 1 (TX and RX).
+The new Nano also has a built-in 6DOF IMU.
+To interface the motor controller with the Nano, connect TX and RX (from motor controller) to pins 1 and 0 (on the Arduino) respectively.
+The code in cp_pid_iot already contains the commands necessary to send motor controls through this hardware serial.
+cp_pid_iot also uses the built-in IMU. The PID gains must be tuned for each unique robot.
---
+++ cp_pid_iot/cp_pid_iot.ino
@@ -0,0 +1,110 @@
+/*
+  CartPole V2 (Arduino Nano 33 IOT)
+*/
+
+#include <Arduino.h>
+#include <Arduino_LSM6DS3.h>
+#include <PID_v1.h>
+
+float RAD2DEG = 180/PI;
+
+float ax, ay, az, gx, gy, gz;
+float theta = 0.0;
+float dtheta = 0.0;
+
+byte cmd[4]; // serial command buffer
+
+long first_time, now_time;
+
+//PID
+//Define Variables we'll be connecting to
+double Setpoint, Input, Output;
+//Define the Tuning Parameters
+double consKp=1.0, consKi=0.0, consKd=0.0;
+//Specify the links and initial tuning parameters
+PID myPID(&Input, &Output, &Setpoint, consKp, consKi, consKd, DIRECT);
+
+//###################################################################
+void setup() {
+
+  // Start hardware serial
+  // Reset the qik
+  digitalWrite(4, LOW);
+  pinMode(4, OUTPUT); // drive low
+  delay(1);
+  pinMode(4, INPUT);  // return to high-impedance input (reset is internally pulled up on qik)
+  delay(10);
+
+  // Initialize motor controller serial
+  Serial1.begin(9600);
+  Serial1.write(0xAA);
+
+  Serial.begin(9600);
+  while (!Serial);
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+
+  Setpoint = 0.0;
+  myPID.SetOutputLimits(-127, 127);       //[-127, 127] tell the PID to range between motor PWM response
+  myPID.SetSampleTime(10);                // Ensure that the PID is called at regular interval  
+  myPID.SetMode(AUTOMATIC);               //turn the PID on 
+  
+  Serial.println("Initialization complete");
+}
+
+
+//###################################################################
+void loop() {
+
+  first_time = millis();
+  now_time = millis();
+
+  while ((now_time - first_time)/1000.0 < 15.0){  
+
+    getAngle();
+    myPID.Compute();
+    setMotors(Output,Output);
+    
+    Serial.print(Input);
+    Serial.print("__:__");
+    Serial.print(Output);
+    Serial.print("__:__");
+    Serial.println((now_time - first_time)/1000.0);
+    
+    now_time = millis();
+  }
+
+  Serial.println("Test ended.");
+  setMotors(0,0);
+  while(1);
+}
+
+//###################################################################
+void getAngle() {
+  if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(ax, ay, az);
+    theta = atan(ax/sqrt(sq(ay)+sq(az)))*RAD2DEG;
+    Input = theta;
+  }
+}
+
+//###################################################################
+void setMotors(int MLeft, int MRight) {
+  if (MLeft>=0){ 
+    cmd[0] = 0x88;
+  }else{
+    cmd[0] = 0x8A;
+  }
+  cmd[1] = abs(MLeft);
+  
+  if (MRight>=0){ 
+    cmd[2] = 0x8C;
+  }else{
+    cmd[2] = 0x8E;
+  }
+  cmd[3] = abs(MRight);
+  Serial1.write(cmd, 4);
+}
